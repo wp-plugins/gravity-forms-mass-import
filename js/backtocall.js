4c8b5ac49c49b08b3dc0c1b79a12e11ec19c68ce
dunar21 <dunar21@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-24 00:54:31 +0000

    *Will now upload large text into proper database table
    *Fixed issue with a query that was querying entire table instead of just a single value
    
    *added support for setting date of creation for an entry
    
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-mass-import/trunk@937594 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/backtocall.js
@@ -0,0 +1,32 @@
+﻿jQuery.noConflict();
+jQuery(document).ready(function(){
+	var thi_val;
+
+	jQuery('.showstuff').hide();	
+	jQuery('#myList').change(function () {
+	
+	thi_val = jQuery(this).val();
+	givebackstuff(thi_val);
+	
+	});
+	
+});
+function givebackstuff(thi_val){
+var jaxrel = fromphp.jaxfile;
+	jQuery("body").css("cursor", "progress");
+	var valset = {
+				action: 'give_backstuff',
+				dataVal: thi_val
+			};
+		jQuery.post(jaxrel, valset,
+		function(data) {
+			jQuery("body").css("cursor", "default");
+			jQuery('.showstuff').hide(300);
+
+			 jQuery('.showstuff').html(data.sucs);
+
+			 jQuery('.showstuff').show(300);
+			}, 
+			"json"
+		);
+}
